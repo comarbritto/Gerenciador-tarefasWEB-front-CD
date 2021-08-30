22f289cec9b868646954bfe67fd7e4b2af60cc1e
Omar Britto <comarbritto@gmail.com>
2021-08-30 17:19:26 -0300

    header criado, porém sem as referências de páginas

--- web/src/View/Home/index.js
+++ web/src/View/Home/index.js
@@ -1,6 +1,15 @@
 
+import React from 'react';
+import * as Styled from './styles';
+import Header from '../../Componentes/Header';
 
 function Home() {
-  return <h1> Você esta na primeira página do CD</h1>
+  return (
+    <Styled.Container>
+      <Header />
+    </Styled.Container>
+  )
 }
+
+/* REPONSÁVEL POR EXPORTAR A FUNÇÃO PARA OUTRAS ÁREA DO CÓDIGO */
 export default Home;
